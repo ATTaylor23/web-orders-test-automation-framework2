236e87becf00a9b5da9c31a5ccaa5c61564b3521
ATTaylor23 <taylorthorntonalexander23@gmail.com>
2022-04-21 22:11:32 -0400

    3rd commit

--- src/test/java/Utils/Driver.java
+++ src/test/java/Utils/Driver.java
@@ -13,13 +13,13 @@ import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
 
-        public static WebDriver driver;
+        public static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();  //supports thread safe WebDrivers
 
         public Driver(){} // to prevent creating objects
 
-        public static WebDriver getDriver(){
+        public static synchronized WebDriver getDriver(){
 
-            if (driver == null) {   //check if the WebDriver is not initialized
+            if (drivers.get() == null) {   //check if the WebDriver is not initialized
                 //String browser = PropertyReader.readProperties("browser").toLowerCase();
 
                 String browser = System.getProperty("browser");
@@ -31,54 +31,54 @@ public class Driver {
                 switch (browser.toLowerCase()) {
                     case "chrome":
                         WebDriverManager.chromedriver().setup();
-                        driver = new ChromeDriver();
+                        drivers.set(new ChromeDriver());
                         break;
                     case "chromeheadless":
                         WebDriverManager.chromedriver().setup();
                         ChromeOptions options = new ChromeOptions();
                         options.addArguments("--headless");
                         options.addArguments("start-maximized");
-                        driver = new ChromeDriver(options);
+                        drivers.set(new ChromeDriver(options));
                         break;
                     case "firefox":
                         WebDriverManager.firefoxdriver().setup();
-                        driver = new FirefoxDriver();
+                        drivers.set(new FirefoxDriver());
                         break;
                     case "firefoxheadless":
                         WebDriverManager.firefoxdriver().setup();
                         FirefoxOptions foptions = new FirefoxOptions();
                         foptions.addArguments("--headless");
-                        driver = new FirefoxDriver(foptions);
+                        drivers.set(new FirefoxDriver(foptions));
                         break;
                     case "edge":
                         WebDriverManager.edgedriver().setup();
-                        driver = new EdgeDriver();
+                        drivers.set(new EdgeDriver());
                         break;
                     case "edgeheadless":
                         WebDriverManager.edgedriver().setup();
                         EdgeOptions edgeOptions = new EdgeOptions();
                         edgeOptions.addArguments("--headless");
-                        driver = new EdgeDriver(edgeOptions);
+                        drivers.set(new EdgeDriver(edgeOptions));
                         break;
                     case "safari":
                         WebDriverManager.safaridriver().setup();
-                        driver = new SafariDriver();
+                        drivers.set(new SafariDriver());
                         break;
                     case "internet explorer":
                         WebDriverManager.iedriver().setup();
-                        driver = new InternetExplorerDriver();
+                        drivers.set(new InternetExplorerDriver());
                         break;
                     default:
                         throw new RuntimeException("Invalid browser");
                 }
             }
-            return driver;
+            return drivers.get();
         }
 
-        public static void quitDriver(){
-            if (driver != null){
-                driver.quit();
-                driver = null;
+        public static synchronized void quitDriver(){
+            if (drivers.get() != null){
+                drivers.get().quit();
+                drivers.remove();
             }
         }
 
